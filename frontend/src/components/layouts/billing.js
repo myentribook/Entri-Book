54117711f4a4a1131c2fe7dc4d6272ebeb9ba163
myentribook <myentribook@gmail.com>
2026-08-12 22:35:17 +0530

    Update billing.js

--- frontend/src/components/layouts/billing.js
+++ frontend/src/components/layouts/billing.js
@@ -81,7 +81,7 @@ export default function Billing() {
     if (!selectedBill) return;
     try {
       setSharing(true);
-      const backendUrl = "http://16.171.148.56:8000";
+      const backendUrl = "http://www.myentribook.in";
       const config = { withCredentials: true }; 
       
       const requestData = {
