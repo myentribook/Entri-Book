ac8c53db321386a18f4a7c949545754b86e3da35
myentribook <myentribook@gmail.com>
2026-08-13 13:45:02 +0530

    Update billing.js

--- frontend/src/components/layouts/billing.js
+++ frontend/src/components/layouts/billing.js
@@ -81,7 +81,71 @@ export default function Billing() {
     if (!selectedBill) return;
     try {
       setSharing(true);
-      const backendUrl = "http://www.myentribook.in";
+      const backendUrl = "
+import axios from 'axios';
+import { 
+    createPaymentRequest, 
+    createPaymentSuccess, 
+    createPaymentFailure, 
+    trailRequest, 
+    trailSuccess, 
+    trailFailure, 
+    verifyPaymentRequest, 
+    verifyPaymentSuccess, 
+    verifyPaymentFailure,
+    subscriptionRequest,
+    subscriptionSuccess,
+    subscriptionFail 
+} from '../slices/paymentSlice';
+
+const API = axios.create({
+    baseURL: 'http://16.171.148.56:8000/',
+    withCredentials: true
+});
+
+export const createPayment = (paymentData) => async (dispatch) => {
+    try {
+        dispatch(createPaymentRequest());
+        const { data } = await API.post(`/api/v1/order`, paymentData);
+        dispatch(createPaymentSuccess(data));
+    } catch (error) {
+        const message = error.response?.data?.message || error.message || `payment failure`;
+        dispatch(createPaymentFailure(message));
+    }
+};
+
+export const trails = (trailData) => async (dispatch) => {
+    try {
+        dispatch(trailRequest());
+        const { data } = await API.post(`/api/v1/trial`, trailData);
+        dispatch(trailSuccess(data));
+    } catch (error) {
+        const message = error.response?.data?.message || error.message || 'Trails creation Failure';
+        dispatch(trailFailure(message));
+    }
+};
+
+export const verifyPayment = (verifyPaymentData) => async (dispatch) => {
+    try {
+        dispatch(verifyPaymentRequest());
+        const { data } = await API.post(`/api/v1/verify`, verifyPaymentData);
+        dispatch(verifyPaymentSuccess(data));
+    } catch (error) {
+        const message = error.response?.data?.message || error.message || `Verify Payment Failure`; 
+        dispatch(verifyPaymentFailure(message));
+    }
+};
+
+export const getMySubscription = () => async (dispatch) => {
+    try {
+        dispatch(subscriptionRequest());
+        const { data } = await API.get(`/api/v1/my-subscription`);
+        dispatch(subscriptionSuccess(data));
+    } catch (error) {
+        const message = error.response?.data?.message || error.message || 'Failed to fetch subscription';
+        dispatch(subscriptionFail(message));
+    }
+};";
       const config = { withCredentials: true }; 
       
       const requestData = {
